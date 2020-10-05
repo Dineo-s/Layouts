4c6da7c61a4dcbeca0306fde0e9a0afa20285960
dineo789 <dineo.thato06@gmail.com>
2020-10-05 23:09:10 +0200

    Initial commit

---
+++ app/src/main/java/com/libraries/android/constraintlayouts/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.libraries.android.constraintlayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
